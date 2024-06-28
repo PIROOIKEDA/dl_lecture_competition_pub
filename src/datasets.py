fc027bab72f98c43b082d13f61c24956e9bfd622
PIROOIKEDA <colabohiroo@gmail.com>
2024-06-28 16:21:03 +0000

    src_3_idx_fail

--- src/datasets.py
+++ src/datasets.py
@@ -1,88 +1,170 @@
 #dataset
+#フーリエ変換とARモデルによる特徴量を追加したEnhancedEEGDatasetを追加
+#chunkに分けてロード
 
 import os
 import torch
 import numpy as np
-from scipy.signal import resample, butter, filtfilt
+from scipy.signal import resample, butter, filtfilt, welch
+from statsmodels.tsa.ar_model import AutoReg
 from torch.utils.data import Dataset
 
-class ThingsMEGDataset(Dataset):
-    def __init__(self, split: str, data_dir: str = "data", new_fs=None, lowcut=None, highcut=None, baseline_correct=False) -> None:
-        """
-        Initialize dataset and load data.
-        """
+class EnhancedEEGDataset(Dataset):
+    def __init__(self, split: str, data_dir: str = "data", chunk_size=5000, new_fs=None, lowcut=None, highcut=None, baseline_correct=False, feature_extraction=False) -> None:
         super().__init__()
 
         assert split in ["train", "val", "test"], f"Invalid split: {split}"
         self.split = split
+        self.data_dir = data_dir
+        self.chunk_size = chunk_size
+        self.new_fs = new_fs
+        self.lowcut = lowcut
+        self.highcut = highcut
+        self.baseline_correct = baseline_correct
+        self.feature_extraction = feature_extraction
+        self.num_subjects = 4
         self.num_classes = 1854
 
-        # Load the data files
-        try:
-            print(f"Loading {split}_X.pt")
-            file_path = os.path.join(data_dir, f"{split}_X.pt")
-            if not os.path.exists(file_path):
-                raise FileNotFoundError(f"{file_path} does not exist.")
-            self.X = torch.load(file_path)
+        # Load metadata
+        self.load_metadata()
 
-            print(f"Loading {split}_subject_idxs.pt")
-            file_path = os.path.join(data_dir, f"{split}_subject_idxs.pt")
-            if not os.path.exists(file_path):
-                raise FileNotFoundError(f"{file_path} does not exist.")
+        # Check if preprocessed data exists, otherwise preprocess and save
+        self.preprocessed_data_path = os.path.join(self.data_dir, f'{self.split}_data.pt') #splitごとに前処理データ保存
+        self.preprocessed_features_path = os.path.join(self.data_dir, f'{self.split}_features.pt') #splitごとに特長量データ保存
+
+        if not os.path.exists(self.preprocessed_data_path):
+            self.preprocess_and_save_data()
+
+        # Load preprocessed data
+        self.load_preprocessed_data()
+
+    def load_metadata(self):
+        try:
+            print(f"Loading {self.split}_X.pt metadata")
+            file_path = os.path.join(self.data_dir, f"{self.split}_X.pt")
+            if not os.path.exists(file_path): #エラーチェック
+                raise FileNotFoundError(f"{file_path} does not exist。") #エラーチェック
+            self.X_metadata = torch.load(file_path)
+
+            print(f"Loading {self.split}_subject_idxs.pt") #エラーチェック
+            file_path = os.path.join(self.data_dir, f"{self.split}_subject_idxs.pt") 
+            if not os.path.exists(file_path): #エラーチェック
+                raise FileNotFoundError(f"{file_path} does not exist。") #エラーチェック
             self.subject_idxs = torch.load(file_path)
 
-            if split in ["train", "val"]:
-                print(f"Loading {split}_y.pt")
-                file_path = os.path.join(data_dir, f"{split}_y.pt")
-                if not os.path.exists(file_path):
-                    raise FileNotFoundError(f"{file_path} does not exist.")
+            if self.split in ["train", "val"]:
+                print(f"Loading {self.split}_y.pt")
+                file_path = os.path.join(self.data_dir, f"{self.split}_y.pt")
+                if not os.path.exists(file_path): #エラーチェック 
+                    raise FileNotFoundError(f"{file_path} does not exist。") #エラーチェック
                 self.y = torch.load(file_path)
-                assert len(torch.unique(self.y)) == self.num_classes, "Number of classes do not match."
 
         except Exception as e:
-            print(f"An error occurred while loading data: {e}")
+            print(f"An error occurred while loading metadata: {e}") #エラーチェック
             raise
 
-        print(f"Loaded dataset: {split} with {len(self.X)} samples.")
+        print(f"Loaded metadata for dataset: {self.split} with {len(self.X_metadata)} samples。") #エラーチェック
 
-    def preprocess(self, new_fs=None, lowcut=None, highcut=None, baseline_correct=False, old_fs=1000):
-        """
-        Preprocess the data.
-        """
-        X_np = self.X.numpy()
-
-        if baseline_correct:
+    def preprocess(self, X_np):
+        if self.baseline_correct:
             baseline = np.mean(X_np, axis=-1, keepdims=True)
-            X_np = X_np - baseline
+            X_np = X_np - baseline 
 
-        if new_fs is not None and new_fs != old_fs:
-            num_samples = int(X_np.shape[-1] * (new_fs / old_fs))
+        if self.new_fs is not None:
+            num_samples = int(X_np.shape[-1] * (self.new_fs / 1000))
             X_np = resample(X_np, num_samples, axis=-1)
 
-        if lowcut is not None:
-            b, a = butter(5, lowcut / (0.5 * new_fs), btype='high')
+        if self.lowcut is not None:
+            b, a = butter(5, self.lowcut / (0.5 * self.new_fs), btype='high')
             X_np = filtfilt(b, a, X_np, axis=-1)
 
-        if highcut is not None:
-            b, a = butter(5, highcut / (0.5 * new_fs), btype='low')
+        if self.highcut is not None:
+            b, a = butter(5, self.highcut / (0.5 * self.new_fs), btype='low')
             X_np = filtfilt(b, a, X_np, axis=-1)
 
-        self.X = torch.tensor(X_np)
+        return X_np
+
+    def preprocess_and_save_data(self):
+        data_list = []
+        features_list = []
+        subject_list = []
+
+        for start_idx in range(0, len(self.X_metadata), self.chunk_size):
+            end_idx = min(start_idx + self.chunk_size, len(self.X_metadata))
+            chunk_data = [self.preprocess(self.X_metadata[i].numpy()) for i in range(start_idx, end_idx)]
+            chunk_data = np.array(chunk_data)  # chunkごとにnp.array型に
+            data_list.append(chunk_data)
+
+            if self.feature_extraction:
+                chunk_features = self.extract_features(chunk_data, self.new_fs)  # フーリエ変換、ARモデル適用
+                features_list.append(chunk_features)  # chunkごとにfeatures_listに追加
+
+            subject_list.append(self.subject_idxs[start_idx:end_idx])
+
+        data = torch.tensor(np.concatenate(data_list, axis=0), dtype=torch.float32)  # データを結合
+        torch.save(data, self.preprocessed_data_path)  # 前処理データを保存
+
+        if self.feature_extraction:
+            features = torch.tensor(np.concatenate(features_list, axis=0), dtype=torch.float32)
+            # 特徴量データにsubject_idxを結合
+            subjects = torch.tensor(np.concatenate(subject_list, axis=0), dtype=torch.long)
+            features_with_subjects = torch.cat((features, self.subject_idxs.float()), dim=1)
+            torch.save(features_with_subjects, self.preprocessed_features_path) #特長量データ保存
+
+    def load_preprocessed_data(self):
+        self.data = torch.load(self.preprocessed_data_path)
+        if self.feature_extraction:
+            self.features = torch.load(self.preprocessed_features_path)
+
+    def extract_features(self, eeg_signals, fs, bands=[(8, 13), (13, 30), (30, 45)], lags=5):
+        def extract_band_power(eeg_signal, fs, band):
+            f, Pxx = welch(eeg_signal, fs=fs, nperseg=256)
+            band_power = np.sum(Pxx[(f >= band[0]) & (f <= band[1])])
+            return band_power
+
+        def extract_ar_features(eeg_signal, lags=5):
+            model = AutoReg(eeg_signal, lags=lags)
+            model_fit = model.fit()
+            ar_params = model_fit.params
+            return ar_params
+
+        all_features = []
+        for signal in eeg_signals:
+            features = []
+            for band in bands:
+                power = extract_band_power(signal, fs, band)
+                features.append(power)
+            ar_features = extract_ar_features(signal, lags)
+            features.extend(ar_features)
+            all_features.append(features)
+        return np.array(all_features)
+
+    def one_hot_encode(self, idx, num_classes):
+        one_hot = torch.zeros(num_classes)
+        one_hot[idx] = 1.0
+        return one_hot
 
     def __len__(self) -> int:
-        return len(self.X)
+        return len(self.data)
 
     def __getitem__(self, i):
-        if hasattr(self, "y"):
-            return self.X[i], self.y[i], self.subject_idxs[i]
+        X = self.data[i]
+        subject_id = self.subject_idxs[i]
+        subject_one_hot = self.one_hot_encode(subject_id, self.num_subjects)
+        if self.feature_extraction:
+            f = self.features[i]
         else:
-            return self.X[i], self.subject_idxs[i]
+            f = subject_one_hot
+        if hasattr(self, "y"):
+            y = self.y[i]
+            return X, f, y
+        else:
+            return X, f
 
     @property
     def num_channels(self) -> int:
-        return self.X.shape[1]
+        return self.data.shape[1]
 
     @property
     def seq_len(self) -> int:
-        return self.X.shape[2]
-
+        return self.data.shape[2]
--- src/models.py
+++ src/models.py
@@ -1,9 +1,11 @@
 #CNN+LSTM
+#特徴量データと被験者idを線形結合で組み込むHybridModelを追加
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from einops.layers.torch import Rearrange
+
 
 class ConvLSTMClassifier(nn.Module):
     def __init__(self, num_classes: int, seq_len: int, in_channels: int, lstm_hidden_dim: int = 128) -> None:
@@ -32,13 +34,13 @@ class ConvLSTMClassifier(nn.Module):
 class ConvBlock(nn.Module):
     def __init__(self, in_dim, out_dim, kernel_size: int = 3, p_drop: float = 0.1) -> None:
         super().__init__()
-        
+
         self.in_dim = in_dim
         self.out_dim = out_dim
 
         self.conv0 = nn.Conv1d(in_dim, out_dim, kernel_size, padding="same")
         self.conv1 = nn.Conv1d(out_dim, out_dim, kernel_size, padding="same")
-        
+
         self.batchnorm0 = nn.BatchNorm1d(num_features=out_dim)
         self.batchnorm1 = nn.BatchNorm1d(num_features=out_dim)
 
@@ -56,3 +58,126 @@ class ConvBlock(nn.Module):
         X = F.gelu(self.batchnorm1(X))
 
         return self.dropout(X)
+
+
+
+class HybridModel(nn.Module):
+    def __init__(self, num_classes: int, seq_len: int, in_channels: int, lstm_hidden_dim: int = 128, feature_dim: int = 4, feature_hid_dim: int = 16, feature_out_dim: int = 4) -> None:
+        super().__init__()
+
+        self.conv_blocks = nn.Sequential(
+            ConvBlock(in_channels, 128),
+            ConvBlock(128, 128)
+        ) #時系列データを処理する畳み込み層（Convは下で定義）の関数
+
+        self.lstm = nn.LSTM(input_size=128, hidden_size=lstm_hidden_dim, batch_first=True, bidirectional=True) #双方向LSTM
+
+        self.feature_fc = nn.Sequential(
+            nn.Linear(feature_dim, feature_hid_dim),
+            nn.ReLU(),
+            nn.Linear(feature_hid_dim, feature_out_dim)
+        ) #特徴量データの線形結合層の関数
+
+        self.head = nn.Sequential(
+            nn.Linear(lstm_hidden_dim * 2 + feature_out_dim, num_classes)  # bidirectional, so hidden_dim * 2 + feature_out_dim
+        ) #出力層の関数：1次元の入力テンソルに対して適応平均プーリング（最後だけだとあまり安定しない？）双方向LSTMと線形層の合計の次元をクラス数に変換
+
+    def forward(self, X: torch.Tensor, f: torch.Tensor) -> torch.Tensor:
+
+        X = self.conv_blocks(X)
+        X = X.permute(0, 2, 1)  # Prepare for LSTM: (batch, time, features)
+        X, _ = self.lstm(X)
+        X = X.mean(dim=1)  # 時系列全体の平均を計算
+        f = self.feature_fc(f)
+
+        combine = self.head(torch.cat((X, f), dim=1))
+        return combine
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_dim, out_dim, kernel_size: int = 3, p_drop: float = 0.1) -> None:
+        super().__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        self.conv0 = nn.Conv1d(in_dim, out_dim, kernel_size, padding="same") #in_dim, out_dim, kernel_size
+        self.conv1 = nn.Conv1d(out_dim, out_dim, kernel_size, padding="same") #out_dim, out_dim, kernel_size
+
+        self.batchnorm0 = nn.BatchNorm1d(num_features=out_dim)
+        self.batchnorm1 = nn.BatchNorm1d(num_features=out_dim)
+
+        self.dropout = nn.Dropout(p_drop)
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        if self.in_dim == self.out_dim:
+            X = self.conv0(X) + X  # skip connection
+        else:
+            X = self.conv0(X)
+
+        X = F.gelu(self.batchnorm0(X))
+
+        X = self.conv1(X) + X  # skip connection
+        X = F.gelu(self.batchnorm1(X))
+
+        return self.dropout(X)
+
+'''
+class HybridModel(nn.Module):
+    def __init__(self, num_classes: int, seq_len: int, in_channels: int, lstm_hidden_dim: int = 128, feature_dim: int = 10) -> None:
+        super().__init__()
+
+        self.conv_blocks = nn.Sequential(
+            ConvBlock(in_channels, 128),
+            ConvBlock(128, 128)
+        ) #時系列データを処理する畳み込み層
+
+        self.lstm = nn.LSTM(input_size=128, hidden_size=lstm_hidden_dim, batch_first=True, bidirectional=True)
+
+        self.feature_fc = nn.Sequential(
+            nn.Linear(feature_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128)
+        ) #特徴量データの線形結合層
+
+        self.head = nn.Linear(lstm_hidden_dim * 2 + 128, num_classes)  # bidirectional, so hidden_dim * 2 + feature_fc output
+
+    def forward(self, X: torch.Tensor, f: torch.Tensor) -> torch.Tensor:
+        X = self.conv_blocks(X) #時系列データを処理する畳み込み層
+        X = X.permute(0, 2, 1)  # Prepare for LSTM: (batch, time, features)
+        _, (X, _) = self.lstm(X)  # Get last hidden state from LSTM
+        X = torch.cat((X[-2,:,:], X[-1,:,:]), dim=1)  # Concatenate the last hidden states from both directions
+
+        f = self.feature_fc(f) #特徴量データの線形結合層
+
+        combined = torch.cat((X, f), dim=1)
+        return self.head(combined)
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_dim, out_dim, kernel_size: int = 3, p_drop: float = 0.1) -> None:
+        super().__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        self.conv0 = nn.Conv1d(in_dim, out_dim, kernel_size, padding="same")
+        self.conv1 = nn.Conv1d(out_dim, out_dim, kernel_size, padding="same")
+
+        self.batchnorm0 = nn.BatchNorm1d(num_features=out_dim)
+        self.batchnorm1 = nn.BatchNorm1d(num_features=out_dim)
+
+        self.dropout = nn.Dropout(p_drop)
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        if self.in_dim == self.out_dim:
+            X = self.conv0(X) + X  # skip connection
+        else:
+            X = self.conv0(X)
+
+        X = F.gelu(self.batchnorm0(X))
+
+        X = self.conv1(X) + X  # skip connection
+        X = F.gelu(self.batchnorm1(X))
+
+        return self.dropout(X)
+'''
