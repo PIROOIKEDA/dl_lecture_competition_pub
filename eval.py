9a14797afb2b7484b7c669b34f4685391061c00d
PIROOIKEDA <colabohiroo@gmail.com>
2024-07-17 13:19:10 +0000

    0.03615main

--- eval.py
+++ eval.py
@@ -10,8 +10,20 @@ from termcolor import cprint
 from tqdm import tqdm
 
 from src.datasets import ThingsMEGDataset
-from src.models import BasicConvClassifier
+from src.datasets import CustomDataLoader
+from src.models import ConvTransformerClassifier
 from src.utils import set_seed
+import yaml
+from hydra.core.global_hydra import GlobalHydra
+from torch.utils.data import Dataset, DataLoader, Subset, Sampler
+from sklearn.model_selection import ParameterGrid
+from sklearn.metrics import accuracy_score
+from torch.utils.data import DataLoader, TensorDataset
+import torch.optim as optim
+import random
+from torch.cuda.amp import GradScaler, autocast
+import bitsandbytes as bnb
+
 
 
 @torch.no_grad()
@@ -19,22 +31,32 @@ from src.utils import set_seed
 def run(args: DictConfig):
     set_seed(args.seed)
     savedir = os.path.dirname(args.model_path)
+
+    data_dir = args.data_dir
     
     # ------------------
     #    Dataloader
-    # ------------------    
-    test_set = ThingsMEGDataset("test", args.data_dir)
-    test_loader = torch.utils.data.DataLoader(
-        test_set, shuffle=False, batch_size=args.batch_size, num_workers=args.num_workers
-    )
+    # ------------------ 
+    def seed_worker(worker_id):
+        worker_seed = torch.initial_seed() % 2**32
+        np.random.seed(worker_seed)
+        random.seed(worker_seed)   
+    g = torch.Generator()
+    g.manual_seed(args.seed)
+
+
+    loader_args = {"num_workers": args.num_workers, "worker_init_fn": seed_worker, "generator": g}
+
+    test_set = ThingsMEGDataset("test", data_dir, front_cut=args.front_cut, tail_cut=args.tail_cut, highcut=args.highcut, pretrain = False)
+
+    test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False, **loader_args)
 
     # ------------------
     #       Model
     # ------------------
-    model = BasicConvClassifier(
-        test_set.num_classes, test_set.seq_len, test_set.num_channels
-    ).to(args.device)
-    model.load_state_dict(torch.load(args.model_path, map_location=args.device))
+    model = ConvTransformerClassifier(test_set.num_classes,test_set.seq_len,test_set.num_channels,out_channels=args.transformer_out_channels,transformer_hidden_dim=args.transformer_hidden_dim,num_heads=args.num_heads,num_layers=args.num_layers,).to(args.device)
+    model.load_state_dict(torch.load(os.path.join(savedir, "model_best.pt"), map_location=args.device))
+
 
     # ------------------
     #  Start evaluation
@@ -42,7 +64,7 @@ def run(args: DictConfig):
     preds = [] 
     model.eval()
     for X, subject_idxs in tqdm(test_loader, desc="Validation"):        
-        preds.append(model(X.to(args.device)).detach().cpu())
+        preds.append(model(X.to(args.device), subject_idxs.to(args.device)).detach().cpu())
         
     preds = torch.cat(preds, dim=0).numpy()
     np.save(os.path.join(savedir, "submission"), preds)
